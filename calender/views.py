ba95d38f9ab87826ed332e44dff2d20ff2034c20
pavanpreetham10 <53636009+pavanpreetham10@users.noreply.github.com>
2020-07-30 20:44:18 +0530

    Update views.py

--- calender/views.py
+++ calender/views.py
@@ -21,29 +21,6 @@ def calender(request):
         return e['startTimeSeconds']
 
     cf_contest_list.sort(key=fun)
-<<<<<<< HEAD
-
-    soup = requests.get('https://www.codechef.com/contests/?itm_medium=navmenu&itm_campaign=allcontests#future-contests')
-    soup = BeautifulSoup(soup.content, 'html')
-    soup = soup.find(id='future-contests')
-    soup = soup.next_sibling.next_sibling
-    soup = soup.find_all('td')
-    result = []
-    links = []
-    for tag in soup:
-        # print (tag.text)
-        result.extend(tag.stripped_strings)
-        link = tag.find('a')
-        if link!= None:
-            link = link.get('href')
-            links.append(link)
-    chef_contest_list = []
-    count = len(links)
-    print(count)
-    for i in range(count):
-        list = []
-=======
-    
     
     soup = requests.get('https://www.codechef.com/contests/?itm_medium=navmenu&itm_campaign=allcontests#future-contests')
     soup=BeautifulSoup(soup.content,'html')
@@ -64,39 +41,21 @@ def calender(request):
     print (count)
     for i in range (count):
         list=[]
->>>>>>> d8d991e1f6f0c2a6a0b6a130288c8a4c3b2e421e
         list.append(links[0])
         links.pop(0)
         result.pop(0)
         for j in range(3):
             list.append(result[0])
             result.pop(0)
-<<<<<<< HEAD
         result.pop(0), result.pop(0)
         chef_contest_list.append(list)
-        print(chef_contest_list)
-        chef_contest_link = "https://www.codechef.com"
-        context = {'cf_contest_list': cf_contest_list,
-                   'string': string,
-                   'cf_contest_link': cf_contest_link,
-                   'chef_contest_list': chef_contest_list,
-                   'chef_contest_link': chef_contest_link }
-=======
-        result.pop(0),result.pop(0)
-        chef_contest_list.append(list)
-        print(chef_contest_list)
-<<<<<<< HEAD
-        chef_context_link = "https://www.codechef.com"
-        context = {'cf_contest_list': cf_contest_list, 'string': string, 'cf_contest_link':cf_contest_link,'chef_contest_list':chef_contest_list,}
->>>>>>> d8d991e1f6f0c2a6a0b6a130288c8a4c3b2e421e
-=======
-        chef_contest_link = "https://www.codechef.com"
-        context = {'cf_contest_list': cf_contest_list, 
-                   'string': string, 
-                   'cf_contest_link':cf_contest_link, 
-                   'chef_contest_list':chef_contest_list, 
-                   'chef_contest_link':chef_contest_link}
->>>>>>> 0003d5a3f3ac9399fcb96ba5dd838e15c51ffbbf
+    print(chef_contest_list)
+    chef_contest_link = "https://www.codechef.com"
+    context = {'cf_contest_list': cf_contest_list, 
+                'string': string, 
+                'cf_contest_link':cf_contest_link, 
+                'chef_contest_list':chef_contest_list, 
+                'chef_contest_link':chef_contest_link}
     return render(request, 'calender.html', context)
 
 
