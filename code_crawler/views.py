62bf1d1e7fb6f88e485d0029daec17d0f8c06df1
vikyath664 <komatire@iitg.ac.in>
2020-07-30 15:47:27 +0530

    analytics are working

--- code_crawler/views.py
+++ code_crawler/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.decorators import login_required
 from . import forms
+import urllib3, json
 
 @login_required(login_url='login')
 def home(request):
@@ -28,26 +29,43 @@ def register(request):
     if request.user.is_authenticated:
         return redirect('home')
     else:
+        form = forms.UserForm()
         if request.method == "POST":
             form = forms.UserForm(request.POST)
             if form.is_valid():
                 form.save()
                 user = form.cleaned_data.get('username')
                 messages.success(request, 'Account was created for ' + user)
-                return redirect('loginpage')
-        context={}
+                return redirect('login')
+        context={'form':form}
         return render(request,'register.html',context)
 
-@login_required(login_url='login')
+#@login_required(login_url='login')
 def analytics(request):
-    return render(request,'home.html')
+    context = {}
+    if request.method == 'POST':
+        handle = request.POST.get('handleinput')
+        url = "https://codeforces.com/api/user.info?handles=" + handle
+        http = urllib3.PoolManager()
+        json_obj = http.request('GET',url)
+        userinfo = json.loads(json_obj.data.decode('utf-8'))
+        status = userinfo['status']
+        if status != 'OK':
+            status = False
+        else:
+            status = True
+            userinfo = userinfo["result"]
+            userinfo = userinfo[0]
+            context={'userinfo':userinfo, 'status':status}
+
+    return render(request,'analytics.html',context)
 
 def resetpass(request):
-    return render(request,'home.html')
+    return render(request,'resetpass.html')
 
 @login_required(login_url='login')
 def contactus(request):
-    return render(request,'home.html')
+    return render(request,'contactus.html')
 
 def developers(request):
     return render(request,'developers.html')
